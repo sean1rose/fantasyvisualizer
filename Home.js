93a6accd87ab56c71840ad94be6b386ce89c2ec0
Sean Rose <sean1rose@gmail.com>
2018-02-05 02:12:15 -0800

    include ant and nivo charting library w/ sample set up and redid navbar menu

--- Home.js
+++ Home.js
@@ -1,17 +1,94 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
-
+import { Button , Responsive} from 'semantic-ui-react';
+// http://nivo.rocks/#/heatmap/
+import { ResponsiveHeatMap } from 'nivo';
+import testdata from './testdata';
+console.log('testdata - ', testdata);
 class Home extends Component {
   render() {
     return (
-      <div className='home-container'>
+      <div style={{height: "75vh", width: "100%"}} className='home-container'>
         <h1>Fantasy Visualizer</h1>
-        <Link className='button' to='/quarterbacks'>
+        {/* <Link className='button' to='/quarterbacks'>
           <Button>
             Quarterbacks            
           </Button>
-        </Link>
+        </Link> */}
+        <ResponsiveHeatMap
+          cellShape='circle'
+          sizeVariation={1}
+          data={testdata}
+          // columns...
+          // keys={[
+          //   "tgt",
+          //   "rec",
+          //   "recyd",
+          //   "rectd"
+          // ]}
+          keys={[
+            "hot dog",
+            "burger",
+            "sandwich",
+            "kebab",
+            "fries",
+            "donut",
+            "junk",
+            "sushi",
+            "ramen",
+            "curry",
+            "udon"
+          ]}
+          indexBy="country"
+          margin={{
+              "top": 100,
+              "right": 60,
+              "bottom": 60,
+              "left": 60
+          }}
+          forceSquare={true}
+          axisTop={{
+              "orient": "top",
+              "tickSize": 5,
+              "tickPadding": 5,
+              "tickRotation": -90,
+              "legend": "",
+              "legendOffset": 36
+          }}
+          axisLeft={{
+              "orient": "left",
+              "tickSize": 5,
+              "tickPadding": 5,
+              "tickRotation": 0,
+              "legend": "country",
+              "legendPosition": "center",
+              "legendOffset": -40
+          }}
+          cellOpacity={1}
+          cellBorderColor="inherit:darker(0.4)"
+          labelTextColor="inherit:darker(1.8)"
+          defs={[
+              {
+                  "id": "lines",
+                  "type": "patternLines",
+                  "background": "inherit",
+                  "color": "rgba(0, 0, 0, 0.1)",
+                  "rotation": -45,
+                  "lineWidth": 4,
+                  "spacing": 7
+              }
+          ]}
+          fill={[
+              {
+                  "id": "lines"
+              }
+          ]}
+          animate={true}
+          motionStiffness={80}
+          motionDamping={9}
+          hoverTarget="cell"
+          cellHoverOthersOpacity={0.25}          
+        />
       </div>
     )
   }
--- Nav.js
+++ Nav.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { NavLink }from 'react-router-dom';
 import { Menu, Dropdown, Container } from 'semantic-ui-react';
+import { Button } from 'antd'
 import styled from "styled-components";
 
 const DropdownMenuStyles = styled(Dropdown.Menu)`
-  min-width: 260px !important;  
+  min-width: 260px !important;
+  display: grid !important;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr; 
 `;
 
 const DropdownItemStyles = styled(Dropdown.Item)`
   display: inline !important;
 `
 
-const teams = [{name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'}];
+const teams = [{name: 'ari', label: 'ARI'}, {name: 'atl', label: 'ATL'}, {name: 'bal', label: 'BAL'}, {name: 'buf', label: 'BUF'},  {name: 'car', label: 'CAR'}, {name: 'chi', label: 'CHI'}, {name: 'cin', label: 'CIN'}, {name: 'cle', label: 'CLE'},  {name: 'dal', label: 'DAL'}, {name: 'den', label: 'DEN'}, {name: 'det', label: 'DET'}, {name: 'gb', label: 'GB'},  {name: 'hou', label: 'HOU'}, {name: 'ind', label: 'IND'}, {name: 'jax', label: 'JAX'}, {name: 'kc', label: 'KC'},  {name: 'lar', label: 'LAR'}, {name: 'mia', label: 'MIA'}, {name: 'min', label: 'MIN'}, {name: 'ne', label: 'NE'},  {name: 'no', label: 'NO'}, {name: 'nyg', label: 'NYG'}, {name: 'nyj', label: 'NYJ'}, {name: 'oak', label: 'OAK'},  {name: 'phi', label: 'PHI'}, {name: 'pit', label: 'PIT'}, {name: 'lac', label: 'LAC'}, {name: 'sea', label: 'SEA'},  {name: 'sf', label: 'SF'}, {name: 'tb', label: 'TB'}, {name: 'ten', label: 'TEN'}, {name: 'was', label: 'WAS'}];
 const positions = [{name: 'qb', label: 'QB'}, {name: 'rb', label: 'RB'}, {name: 'wr', label: 'WR'}, {name: 'te', label: 'TE'}];
 
 const Nav = () => (
@@ -20,43 +24,35 @@ const Nav = () => (
       <Menu.Item  header>
         <NavLink activeClassName='active' to='/'>Fantasy Visualizer</NavLink>
       </Menu.Item>
-      <Dropdown item simple text='Menu'>
-
-        <Dropdown.Menu>
-
-          <Dropdown.Header>Options</Dropdown.Header>
-
-          <Dropdown.Divider />
-
-          <Dropdown.Item>
-            <i className='dropdown icon'/>
-            <span className='text'>Teams</span>
-            <DropdownMenuStyles>
-              {teams.map((team, idx) => {
-                return (
-                  <DropdownItemStyles key={team.name}>{team.label}</DropdownItemStyles>
-                );
-              })}            
-            </DropdownMenuStyles>          
-          </Dropdown.Item>
-
-          <Dropdown.Divider />
-
-          <Dropdown.Item>
-            <i className='dropdown icon'/>
-            <span className='text'>Positions</span>
-            <Dropdown.Menu>
-              {positions.map((position, idx) => {
-                return (
-                  <Dropdown.Item key={position.name}>{position.label}</Dropdown.Item>
-                );
-              })}            
-            </Dropdown.Menu>          
-          </Dropdown.Item>
-
-        </Dropdown.Menu>
-
+      {/* TEAMS */}
+      <Dropdown item simple text='Teams'>
+        <DropdownMenuStyles>
+          {teams.map((team, idx) => {
+            return (
+              <DropdownItemStyles key={team.name}>{team.label}</DropdownItemStyles>
+            );
+          })}            
+        </DropdownMenuStyles>   
       </Dropdown>
+      
+      {/* POSITIONs */}
+      <Dropdown item simple text='Position'>
+        <Dropdown.Menu>
+          {positions.map((position, idx) => {
+            return (
+              <Dropdown.Item key={position.name}>{position.label}</Dropdown.Item>
+            );
+          })}            
+        </Dropdown.Menu>     
+      </Dropdown>
+
+      {/* WEEKS */}
+      <Dropdown item simple text='Weekly'>
+      </Dropdown>
+
+      {/* YEARS */}
+      <Dropdown item simple text='Year'>
+      </Dropdown>      
     </Container>
   </Menu>
   // <ul className='nav'>
