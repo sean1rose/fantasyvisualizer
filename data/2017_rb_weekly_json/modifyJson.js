f9581821d7d44d0b589d2743e2537a33fe1f14f7
Sean Rose <sean1rose@gmail.com>
2018-03-05 01:47:42 -0800

    add team and rzopp data

--- data/2017_rb_weekly_json/modifyJson.js
+++ data/2017_rb_weekly_json/modifyJson.js
@@ -1,12 +1,19 @@
 var fs = require('fs');
-var fileName = './data (2).json';
+// var fileName = './data (2).json';
+var fileName = './week_17.json';
 var file = require(fileName);
 
 for (var key in file) {
-  console.log('file[key] - ', file[key]);
-  file[file[key]["Col Player 1"]] = file[key];
-  delete file[key];
+  file[key]["RZ Opp In20"] = file[key]["Rush RZ In20"] + file[key]["Tar RZ In20"];
+  file[key]["RZ Opp In10"] = file[key]["Rush RZ In10"] + file[key]["Tar RZ In10"];
+  file[key]["RZ Opp In5"] = file[key]["Rush RZ In5"] + file[key]["Tar RZ In5"];
 }
+
+// for (var key in file) {
+//   console.log('file[key] - ', file[key]);
+//   file[file[key]["Col Teamname"]] = file[key];
+//   delete file[key];
+// }
 
 fs.writeFile(fileName, JSON.stringify(file, null, 2), function (err) {
   if (err) return console.log(err);
