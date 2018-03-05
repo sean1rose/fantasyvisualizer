a824242857b1c66d743defaf673cb6009202a9db
Sean Rose <sean1rose@gmail.com>
2018-03-05 02:30:56 -0800

    got a bar chart on the screen

--- Home.js
+++ Home.js
@@ -1,97 +1,86 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { Link } from 'react-router-dom';
 import { Button , Responsive} from 'semantic-ui-react';
-// http://nivo.rocks/#/heatmap/
-import { ResponsiveHeatMap } from 'nivo';
-import testdata from './testdata';
-console.log('testdata - ', testdata);
+import { ResponsiveHeatMap, Pie } from 'nivo';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend }  from 'recharts';
+
+import rbDataService from './services/rbDataservice';
+import teamDataService from './services/teamDataService';
+console.log('Kareem Hunt - ', rbDataService.getRbAllWeeks('Hunt Kareem'));
+console.log('KC Chiefs - ', teamDataService.getTeamAllWeeks('Kansas City Chiefs'));
+
+const kareem = rbDataService.getRbAllWeeks('Hunt Kareem').arr;
+console.log('kareem - ', kareem);
 class Home extends Component {
+
   render() {
     return (
-      <div style={{height: "75vh", width: "100%"}} className='home-container'>
+      <div style={{height: "100vh", width: "100%"}} className='home-container'>
         <h1>Fantasy Visualizer</h1>
         {/* <Link className='button' to='/quarterbacks'>
           <Button>
             Quarterbacks            
           </Button>
         </Link> */}
-        <ResponsiveHeatMap
-          cellShape='circle'
-          sizeVariation={1}
-          data={testdata}
-          // columns...
-          // keys={[
-          //   "tgt",
-          //   "rec",
-          //   "recyd",
-          //   "rectd"
-          // ]}
-          keys={[
-            "hot dog",
-            "burger",
-            "sandwich",
-            "kebab",
-            "fries",
-            "donut",
-            "junk",
-            "sushi",
-            "ramen",
-            "curry",
-            "udon"
-          ]}
-          indexBy="country"
-          margin={{
-              "top": 100,
-              "right": 60,
-              "bottom": 60,
-              "left": 60
-          }}
-          forceSquare={true}
-          axisTop={{
-              "orient": "top",
-              "tickSize": 5,
-              "tickPadding": 5,
-              "tickRotation": -90,
-              "legend": "",
-              "legendOffset": 36
-          }}
-          axisLeft={{
-              "orient": "left",
-              "tickSize": 5,
-              "tickPadding": 5,
-              "tickRotation": 0,
-              "legend": "country",
-              "legendPosition": "center",
-              "legendOffset": -40
-          }}
-          cellOpacity={1}
-          cellBorderColor="inherit:darker(0.4)"
-          labelTextColor="inherit:darker(1.8)"
-          defs={[
-              {
-                  "id": "lines",
-                  "type": "patternLines",
-                  "background": "inherit",
-                  "color": "rgba(0, 0, 0, 0.1)",
-                  "rotation": -45,
-                  "lineWidth": 4,
-                  "spacing": 7
-              }
-          ]}
-          fill={[
-              {
-                  "id": "lines"
-              }
-          ]}
-          animate={true}
-          motionStiffness={80}
-          motionDamping={9}
-          hoverTarget="cell"
-          cellHoverOthersOpacity={0.25}          
-        />
+        <BarChart width={900} height={400} data={kareem} >
+          <XAxis dataKey="week"/>
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3"/>
+          <Tooltip />
+          <Legend />
+          {/* <Bar dataKey="Rz Opp In20" stackId="a" fill="#8884d8" /> */}
+          <Bar dataKey="Att" stackId="a" fill="#82ca9d" />
+          {/* <Bar datakey="Att" fill="#82ca9d" /> */}
+          {/* <Bar stackId="a" /> */}
+        </BarChart>
+
+
       </div>
     )
   }
 }
 
 export default Home;
+
+{/* <Pie
+width={600}
+height={600}
+  data={testdata}
+  margin={{
+      "top": 40,
+      "right": 80,
+      "bottom": 80,
+      "left": 80
+  }}
+  innerRadius={0.5}
+  padAngle={0.7}
+  cornerRadius={3}
+  colors="d320c"
+  colorBy="id"
+  borderColor="inherit:darker(0.6)"
+  radialLabelsSkipAngle={10}
+  radialLabelsTextXOffset={6}
+  radialLabelsTextColor="#333333"
+  radialLabelsLinkOffset={0}
+  radialLabelsLinkDiagonalLength={16}
+  radialLabelsLinkHorizontalLength={24}
+  radialLabelsLinkStrokeWidth={1}
+  radialLabelsLinkColor="inherit"
+  slicesLabelsSkipAngle={10}
+  slicesLabelsTextColor="#333333"
+  animate={true}
+  motionStiffness={90}
+  motionDamping={15}
+  legends={[
+      {
+          "anchor": "bottom",
+          "direction": "row",
+          "translateY": 56,
+          "itemWidth": 100,
+          "itemHeight": 14,
+          "symbolSize": 14,
+          "symbolShape": "circle"
+      }
+  ]}
+/> */}
---
+++ services/teamDataService.js
@@ -0,0 +1,59 @@
+const week1 = require('../data/2017_team_weekly_json/week_1.json');
+const week2 = require('../data/2017_team_weekly_json/week_2.json');
+const week3 = require('../data/2017_team_weekly_json/week_3.json');
+const week4 = require('../data/2017_team_weekly_json/week_4.json');
+const week5 = require('../data/2017_team_weekly_json/week_5.json');
+const week6 = require('../data/2017_team_weekly_json/week_6.json');
+const week7 = require('../data/2017_team_weekly_json/week_7.json');
+const week8 = require('../data/2017_team_weekly_json/week_8.json');
+const week9 = require('../data/2017_team_weekly_json/week_9.json');
+const week10 = require('../data/2017_team_weekly_json/week_10.json');
+const week11 = require('../data/2017_team_weekly_json/week_11.json');
+const week12 = require('../data/2017_team_weekly_json/week_12.json');
+const week13 = require('../data/2017_team_weekly_json/week_13.json');
+const week14 = require('../data/2017_team_weekly_json/week_14.json');
+const week15 = require('../data/2017_team_weekly_json/week_15.json');
+const week16 = require('../data/2017_team_weekly_json/week_16.json');
+const week17 = require('../data/2017_team_weekly_json/week_17.json');
+
+const weeks = {
+  week1: week1,
+  week2: week2,
+  week3: week3,
+  week4: week4,
+  week5: week5,
+  week6: week6,
+  week7: week7,
+  week8: week8,
+  week9: week9,
+  week10: week10,
+  week11: week11,
+  week12: week12,
+  week13: week13,
+  week14: week14,
+  week15: week15,
+  week16: week16,
+  week17: week17
+}
+
+module.exports = {
+  getTeamAllWeeks: (name) => {
+    console.log('name - ', name);
+    let teamDataObj = {};
+    let teamDataArr = [];
+    let counter = 1;
+    for (var key in weeks) {
+      console.log('weeks[key][name] - ', weeks[key][name]);
+      if (weeks[key][name]) {
+        weeks[key][name]["week"] = counter;
+        counter++;      
+      }
+      teamDataObj[key] = weeks[key][name];
+      teamDataArr.push(weeks[key][name]);
+    }
+    return {
+      obj: teamDataObj,
+      arr: teamDataArr
+    };
+  }
+};
